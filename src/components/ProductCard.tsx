f6ed6a985c57a2f8b4aba4134a86f5b97494180b
Daniel Braga <danielbraga2121@gmail.com>
2022-09-12 17:32:24 -0300

    finished-project

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -6,6 +6,7 @@ import React, { useState } from "react"
 import { Button, Card, CardBody, CardSubtitle } from "reactstrap"
 import SuccessToast from "./SucessToast"
 import { ProductType } from "./services/products"
+import { useCart } from "../hooks/useCart"
 
 
 type ProductCardProps = {
@@ -15,6 +16,7 @@ type ProductCardProps = {
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
   const [toastIsOpen, setToastIsOpen] = useState(false)
   const { id, name, imageUrl, price } = product
+  const { addProduct } = useCart()
 
   return (
     <>
@@ -39,6 +41,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
           className="pb-2"
           block
           onClick={() => {
+            addProduct(product)
             setToastIsOpen(true)
             setTimeout(() => setToastIsOpen(false), 1000 * 3)
           }}
